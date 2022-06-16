d043351f960ed4875f7f36d7b1211f2f1192314a
adriel vinicius <adriel1234.1234@gmail.com>
2022-06-16 19:48:35 -0400

    Produtor e associacao muitos para muitos

--- src/main/java/com/empresa/cursomc/domain/Categoria.java
+++ src/main/java/com/empresa/cursomc/domain/Categoria.java
@@ -1,12 +1,15 @@
 package com.empresa.cursomc.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 
 @Entity
@@ -16,10 +19,11 @@ public class Categoria implements Serializable {
 	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private Integer id;
-	
-	
+	private Integer id;	
 	private String nome;
+	
+	@ManyToMany(mappedBy="categorias")
+	private List<Produto> produtos = new ArrayList<>();
 	
 	public Categoria() {
 	}
@@ -62,6 +66,14 @@ public class Categoria implements Serializable {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
 	}
 	
 	
